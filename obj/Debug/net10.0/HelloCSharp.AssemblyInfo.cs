3d6856baf76ee2e197482533b7c7a7bb14a0e2ec
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-01-29 19:08:00 +0530

    add sourcelink.json and update AssemblyInfo with informational version

--- obj/Debug/net10.0/HelloCSharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/HelloCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65226441bb0ec5dda71bb992760b517aef473e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
