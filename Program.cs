38f2331cfc6b0c65ae0e4195cb49beb5cb22e852
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-01-30 22:07:08 +0530

    Refactor : Add Program.cs with basic structure to initialize Calculator

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Calculator calculator = new Calculator();
+
+            calculator.Run();
+        }
+    }
+}
